95f3dc67122857d67aa3d5897715830c47ad4ced
Rohit Reddy S <rsulugur@asu.edu>
2024-08-06 14:27:04 -0400

    Added Pagination Support

--- src/app/home/search-results/search-result/search-result.component.ts
+++ src/app/home/search-results/search-result/search-result.component.ts
@@ -2,16 +2,20 @@ import { Component, input, InputSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TruncatePipe } from './search-result.pipe';
 import { Product } from './search-result.model';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { generateRandomString } from 'src/app/utils/apputils';
 
 @Component({
   selector: 'app-search-result',
   standalone: true,
-  imports: [CommonModule, TruncatePipe],
+  imports: [CommonModule, TruncatePipe, NgxPaginationModule],
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.css']
 })
 export class SearchResultComponent {
   product: InputSignal<Product> = input.required();
+  randomid: string = generateRandomString(5);
+  p: number = 1;
   sortDirection: { 'price': boolean } = { 'price': true };
 
   sortProduct(sortKey: 'price'): void {
---
+++ src/app/utils/apputils.ts
@@ -0,0 +1,9 @@
+export function generateRandomString(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
